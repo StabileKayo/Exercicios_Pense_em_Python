51573a61963bc483d82a85c4ff3650f2b9a614eb
Kayo Stabile <kayolestabile@gmail.com>
2026-01-17 09:29:31 -0300

    Cap 2 - Ex. 1 (2.2)

---
+++ Capitulo 2/Exercício 2.2/Exercício 1.py
@@ -0,0 +1,10 @@
+#Exercício 2.2
+
+#1.	O volume de uma esfera com raio r é (4/3)*pi*r³ . Qual é o volume de uma esfera com raio 5?
+
+from math import pi
+
+raio = 5
+volume = (4/3)* pi * raio **3
+
+print(f"o volume para raio = {raio} é {volume:.2f}")
