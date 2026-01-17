3ba7c8c43c8e296612769fd665dd68d3693bf408
Kayo Stabile <kayolestabile@gmail.com>
2026-01-17 07:55:59 -0300

    Cap 1 - Ex. 1 (1.2)

---
+++ Capitulo 1/Exercício 1.2/Exercicio 1.py
@@ -0,0 +1,10 @@
+#Exercicio 1.2
+
+# 1.	Quantos	segundos há	em 42 minutos e 42 segundos?
+
+minutos = 42
+segundos = 42
+
+total_segundos = segundos + (minutos * 60)
+
+print(f"42 Minutos e 42 segundos são equivalentes a: {total_segundos} segundos" )
