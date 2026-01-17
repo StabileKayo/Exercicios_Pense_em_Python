eafa4e04cc8b92638d1c2d6bbc948fc8ac395ff6
Kayo Stabile <kayolestabile@gmail.com>
2026-01-17 08:04:37 -0300

    Cap 1 - Ex. 2 (1.2)

---
+++ Capitulo 1/Exercício 1.2/Exercício 2.py
@@ -0,0 +1,10 @@
+#Exercício 1.2
+
+#2.	Quantas	milhas há em 10 quilômetros? Dica: uma milha equivale a 1,61 quilômetro?
+
+km_p_milha = 1.61
+km = 10
+
+total_milhas = km_p_milha / km
+
+print(f"{km} quilômetros é equivalente á {total_milhas} milhas")
